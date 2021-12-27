8a32d738df9adf85b8514a0d172825ca9b7af8ad
Helya <z>
2021-12-27 15:12:38 +0300

    Initial commit

---
+++ src/main/java/app/controller/AdminController.java
@@ -0,0 +1,90 @@
+package app.controller;
+
+import app.model.Role;
+import app.model.User;
+import app.service.RoleService;
+import app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("user", userService.getUserList());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.show(id));
+        return "show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String createUser(@ModelAttribute("user") User user,
+                             @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new";
+        }
+        Set<Role> roleSet = new HashSet<>();
+        for (String role : checkBoxRoles) {
+            roleSet.add(roleService.getRoleByName(role));
+        }
+        user.setRoles(roleSet);
+        userService.save(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id) {
+        model.addAttribute("user", userService.show(id));
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user,
+                         @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "admin/edit";
+        }
+        Set<Role> roleSet = new HashSet<>();
+        for (String role : checkBoxRoles) {
+            roleSet.add(roleService.getRoleByName(role));
+        }
+        user.setRoles(roleSet);
+        userService.update(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userService.delete(id);
+        return "redirect:/admin";
+    }
+}
